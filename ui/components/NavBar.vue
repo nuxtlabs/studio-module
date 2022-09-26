f33bb0fbe11a0a1b652c6fbdf258a80680a4a540
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-26 12:15:38 +0800

    feat: update navbar

--- ui/components/NavBar.vue
+++ ui/components/NavBar.vue
@@ -2,10 +2,16 @@
 </script>
 
 <template>
-  <div class="flex justify-between items-center p-4 mb-2">
-    <a @click="setStudioView('Home')">
-      <Logo class="h-5" alt="Nuxt Studio" />
-    </a>
-    <ColorModeToggle class="h-4" />
+  <div class="flex justify-between items-center p-2 max-h-[3rem] min-h-[3rem]">
+    <slot>
+      <div class="px-2">
+        <a @click="setStudioView('Home')">
+          <Logo class="h-5" alt="Nuxt Studio" />
+        </a>
+      </div>
+    </slot>
+    <div class="flex gap-2 items-center h-full px-2">
+      <ColorModeToggle />
+    </div>
   </div>
 </template>
