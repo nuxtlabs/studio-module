4c7d8d17404f506b503feda57f2bd0342a9e5d7a
Farnabaz <farnabaz@gmail.com>
2022-11-21 10:01:54 +0100

    feat(preview): send events through message to studio (#65)

--- src/runtime/plugins/iframe.client.ts
+++ src/runtime/plugins/iframe.client.ts
@@ -6,8 +6,35 @@ export default defineNuxtPlugin(() => {
   if (!window.parent || window.self === window.parent) {
     return
   }
-
   const router = useRouter()
+
+  window.addEventListener('keydown', (e) => {
+    if (e.metaKey || e.ctrlKey || e.altKey || e.shiftKey) {
+      window.parent.postMessage({
+        type: 'nuxt-studio:preview:keydown',
+        payload: {
+          key: e.key,
+          metaKey: e.metaKey,
+          ctrlKey: e.ctrlKey,
+          shiftKey: e.shiftKey,
+          altKey: e.altKey
+        }
+      }, '*')
+    }
+  })
+
+  router?.afterEach((to: any, from) => {
+    window.parent.postMessage({
+      type: 'nuxt-studio:preview:route-changed',
+      payload: {
+        path: to.path,
+        query: to.query,
+        params: to.params,
+        fullPath: to.fullPath
+      }
+    }, '*')
+  })
+
   let editor: NuxtStudioEditor | undefined
 
   const client: NuxtStudioClient = {
@@ -20,8 +47,4 @@ export default defineNuxtPlugin(() => {
   }
 
   window.__NUXT_STUDIO__ = client
-
-  router?.afterEach((to: any) => {
-    editor?.onRouteChanged(to.path)
-  })
 })
