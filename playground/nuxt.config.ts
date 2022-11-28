be6701bccb78083e4da38af77a914163c5648e20
Ahad Birang <farnabaz@gmail.com>
2022-11-28 12:06:49 +0100

    chore: comment

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    // TODO: module functions are not supported yet
     '../src/module'
   ],
   content: {
