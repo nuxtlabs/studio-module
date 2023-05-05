c25b511d63f9c0d32320c106e10e1b44ba63548d
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-05-05 11:48:51 +0200

    up

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,11 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   extends: '@nuxt-themes/alpine',
-  $schema: {
-    appConfig: {
-      configFromNuxtConfig: true
-    }
-  },
   modules: [
     // TODO: module functions are not supported yet
     '../src/module'
--- playground/nuxt.schema.ts
+++ playground/nuxt.schema.ts
@@ -1,8 +1,3 @@
 import { field } from '../src/theme'
 
-export default defineNuxtSchema({
-  appConfig: {
-    someConfig: field({ type: 'string', default: 'schema default' }),
-    configFromNuxtSchema: field('boolean', true)
-  }
-})
+export default defineNuxtSchema({})
