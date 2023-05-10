834ec082a0f2e481f13fbe3d1b2eee7ad1397764
Yaël Guilloux <yael.guilloux@gmail.com>
2023-05-10 10:40:51 +0200

    chore(up): up

--- playground/nuxt.schema.ts
+++ playground/nuxt.schema.ts
@@ -1,3 +1,8 @@
 import { field } from '../src/theme'
 
-export default defineNuxtSchema({})
+export default defineNuxtSchema({
+  appConfig: {
+    someConfig: field({ type: 'string', default: 'schema default' }),
+    configFromNuxtSchema: field('boolean', true)
+  }
+})
